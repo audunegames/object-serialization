93ef4160f16515b286f9d19a97c4c630a6f30154
Danae Dekker <hello@danaedekker.com>
2026-05-26 20:33:33 +0200

    Added extension methods to convert from enumerables to list and object states and vice versa

--- Runtime/Serializers/SerializableExtensions.cs
+++ Runtime/Serializers/SerializableExtensions.cs
@@ -1,43 +1,162 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Audune.Serialization
 {
   // Class that defines extension methods for serialization
   public static class SerializableExtensions
   {
-    #region Serializing and deserializing enumerables
-    // Serialize all elements in an enumerable to an object state
-    public static ObjectState SerializeWithKey<T>(this ISerializationContext context, IEnumerable<T> enumerable, Func<T, string> keySelector)
+    #region Serializing enumerables to list states
+    // Serialize all objects in an enumerable to a list state
+    public static ListState SerializeToList<T>(this ISerializationContext context, IEnumerable<T> enumerable)
     {
       if (context == null)
         throw new ArgumentNullException(nameof(context));
       if (enumerable == null)
-          throw new ArgumentNullException(nameof(enumerable));
-      if (keySelector == null)
-        throw new ArgumentNullException(nameof(keySelector));
-
-      var state = new ObjectState();
+        throw new ArgumentNullException(nameof(enumerable));
+      
+      var state = new ListState();
       foreach (var e in enumerable)
-      {
-        var key = keySelector(e);
-        state.Set(key, context.Serialize(e));
-      }
-
+        state.Add(context.Serialize(e));
+      
       return state;
     }
-
-    // Serialize all objects in an enumerable to an object state
-    public static ObjectState SerializeWithKey<T>(this ISerializationContext context, IEnumerable<T> enumerable) where T : UnityEngine.Object
-    {
-      return SerializeWithKey(context, enumerable, e => e.name);
-    }
-
-    // Deserialize all elements in an enumerable from an object state
-    public static void DeserializeWithKey<T>(this IDeserializationContext context, IEnumerable<T> enumerable, Func<T, string> keySelector, ObjectState state)
+    #endregion
+    
+    #region Deserializing enumerables from list states
+    // Deserialize all items in a list state to an enumerable
+    public static IEnumerable<T> DeserializeFromList<T>(this IDeserializationContext context, IListState state)
     {
       if (context == null)
         throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
+      
+      foreach (var e in state)
+        yield return context.Deserialize<T>(e);
+    }
+
+    
+    // Deserialize all items in a list state and add them into the specified list
+    public static void AddDeserializationFromList<T>(this IDeserializationContext context, IListState state, List<T> list)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
+      if (list == null)
+        throw new ArgumentNullException(nameof(list));
+      
+      foreach (var e in DeserializeFromList<T>(context, state))
+        list.Add(e);
+    }
+    
+    // Deserialize all items in a list state and add them into the specified dictionary with the specified key selector
+    public static void AddDeserializationFromList<T, TKey>(this IDeserializationContext context, IListState state, Dictionary<TKey, T> dictionary, Func<T, TKey> keySelector)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
+      if (dictionary == null)
+        throw new ArgumentNullException(nameof(dictionary));
+      if (keySelector == null)
+        throw new ArgumentNullException(nameof(keySelector));
+      
+      foreach (var e in DeserializeFromList<T>(context, state))
+        dictionary.Add(keySelector(e), e);
+    }
+    #endregion
+    
+    #region Serializing enumerables to object states
+    // Serialize all objects in an enumerable to an object state
+    public static ObjectState SerializeToObject<T>(this ISerializationContext context, IEnumerable<KeyValuePair<string, T>> enumerable)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (enumerable == null)
+        throw new ArgumentNullException(nameof(enumerable));
+      
+      var state = new ObjectState();
+      foreach (var e in enumerable)
+        state.Set(e.Key, context.Serialize(e.Value));
+      
+      return state;
+    }
+    
+    // Serialize all objects in an enumerable to an object state by using the specified key selector
+    public static ObjectState SerializeToObject<T>(this ISerializationContext context, IEnumerable<T> enumerable, Func<T, string> keySelector)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (enumerable == null)
+        throw new ArgumentNullException(nameof(enumerable));
+      if (keySelector == null)
+        throw new ArgumentNullException(nameof(keySelector));
+      
+      return SerializeToObject(context, enumerable.Select(e => KeyValuePair.Create(keySelector(e), e)));
+    }
+    
+    // Serialize all Unity objects in an enumerable to an object state
+    public static ObjectState SerializeToObject<T>(this ISerializationContext context, IEnumerable<T> enumerable) where T : UnityEngine.Object
+    {
+      return SerializeToObject(context, enumerable, e => e.name);
+    }
+    #endregion
+    
+    #region DeDeserializing enumerables from object states
+    // Deserialize all items in an object state to an enumerable
+    public static IEnumerable<KeyValuePair<string, T>> DeserializeFromObject<T>(this IDeserializationContext context, IObjectState state)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
+      
+      foreach (var e in state)
+        yield return KeyValuePair.Create(e.Key, context.Deserialize<T>(e.Value));
+    }
+    
+    
+    // Deserialize all items in an object state and add them into the specified dictionary
+    public static void AddDeserializationFromObject<T>(this IDeserializationContext context, IObjectState state, Dictionary<string, T> dictionary)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
+      if (dictionary == null)
+        throw new ArgumentNullException(nameof(dictionary));
+      
+      foreach (var e in DeserializeFromObject<T>(context, state))
+        dictionary.Add(e.Key, e.Value);
+    }
+    
+    // Deserialize all items in an object state and add them into the specified dictionary with the specified key selector
+    public static void AddDeserializationFromObject<T, TKey>(this IDeserializationContext context, IObjectState state, Dictionary<TKey, T> dictionary, Func<string, TKey> keySelector)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
+      if (dictionary == null)
+        throw new ArgumentNullException(nameof(dictionary));
+      if (keySelector == null)
+        throw new ArgumentNullException(nameof(keySelector));
+      
+      foreach (var e in DeserializeFromObject<T>(context, state))
+        dictionary.Add(keySelector(e.Key), e.Value);
+    }
+    
+    
+    // Deserialize all items in an object state and apply them to the specified enumerable of objects
+    public static void ApplyDeserializationFromObject<T>(this IDeserializationContext context, IObjectState state, IEnumerable<T> enumerable, Func<T, string> keySelector)
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
       if (enumerable == null)
         throw new ArgumentNullException(nameof(enumerable));
       if (keySelector == null)
@@ -51,10 +170,43 @@ namespace Audune.Serialization
       }
     }
 
-    // Deserialize all objects in an enumerable from an object state
+    // Deserialize all items in an object state and apply them to the specified enumerable of Unity objects
+    public static void ApplyDeserializationFromObject<T>(this IDeserializationContext context, ObjectState state, IEnumerable<T> enumerable) where T : UnityEngine.Object
+    {
+      if (context == null)
+        throw new ArgumentNullException(nameof(context));
+      if (state == null)
+        throw new ArgumentNullException(nameof(state));
+      if (enumerable == null)
+        throw new ArgumentNullException(nameof(enumerable));
+      
+      ApplyDeserializationFromObject(context, state, enumerable, e => e.name);
+    }
+    #endregion
+    
+    #region Obsolete methods
+    [Obsolete("Use SerializeToObject instead", false)]
+    public static ObjectState SerializeWithKey<T>(this ISerializationContext context, IEnumerable<T> enumerable, Func<T, string> keySelector)
+    {
+      return SerializeToObject(context, enumerable, keySelector);
+    }
+
+    [Obsolete("Use SerializeToObject instead", false)]
+    public static ObjectState SerializeWithKey<T>(this ISerializationContext context, IEnumerable<T> enumerable) where T : UnityEngine.Object
+    {
+      return SerializeToObject(context, enumerable);
+    }
+
+    [Obsolete("Use ApplyDeserializationFromObject instead", false)]
+    public static void DeserializeWithKey<T>(this IDeserializationContext context, IEnumerable<T> enumerable, Func<T, string> keySelector, ObjectState state)
+    {
+      ApplyDeserializationFromObject(context, state, enumerable, keySelector);
+    }
+
+    [Obsolete("Use ApplyDeserializationFromObject instead", false)]
     public static void DeserializeWithKey<T>(this IDeserializationContext context, IEnumerable<T> enumerable, ObjectState state) where T : UnityEngine.Object
     {
-      DeserializeWithKey(context, enumerable, e => e.name, state);
+      ApplyDeserializationFromObject(context, state, enumerable);
     }
     #endregion
   }
